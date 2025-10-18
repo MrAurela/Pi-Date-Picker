36fb352fdbac547d475ed7f4b9f2cc92e9a7e88c
MrAurela <miroaurela@gmail.com>
2025-10-18 14:40:42 +0300

    Digit selection is updated in UI

--- code.js
+++ code.js
@@ -3,7 +3,12 @@ var currentRow = 0;
 
 document.getElementById("button-up").onclick = onUpButtonPressed;
 document.getElementById("button-down").onclick = onDownButtonPressed;
-document.getElementById("pi-digits-list").onselect = logSelection;
+document.getElementById("pi-digits-list").onselect = onSelectDigits;
+
+document.getElementById("pi-digits-list").focus();
+document.getElementById("pi-digits-list").setSelectionRange(2, 8);
+
+var selectedDateText = document.getElementById("selected-date");
 
 function onUpButtonPressed()  {
     currentRow = Math.max(0, currentRow-5);
@@ -17,10 +22,21 @@ function onDownButtonPressed()  {
     textView.scrollTop = currentRow * 25;
 }
 
-function logSelection(event) {
+function onSelectDigits(event) {
   const selection = event.target.value.substring(
     event.target.selectionStart,
     event.target.selectionEnd,
   );
-  alert(`You selected: ${selection}`);
+  
+  var days = selection.substring(0,2);
+  var months = selection.substring(2,4);
+  var years = selection.substring(4);
+
+  var dateString = days + "/" + months + "/" + years;
+
+  if (true) {
+    selectedDateText.innerText = dateString;
+  } else {
+    alert(`Invalid date: ${selection}`);
+  }
 }
