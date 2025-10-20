ebc0d31b35889d0775363581cefdd6c9cdb63cef
MrAurela <miroaurela@gmail.com>
2025-10-20 09:40:59 +0300

    Created files with all existing PI6 and PI8 dates

--- check_all_date_possible.py
+++ check_all_date_possible.py
@@ -1,4 +1,5 @@
 
+# List all days of the form DD/MM/YY:
 days = range(1,32)
 months = range(1,13)
 years = range(0,100)
@@ -7,38 +8,35 @@ dates = {}
 for year in years:
     for month in months:
         for day in days:
-            dates[f"{str(day).zfill(2)}{str(month).zfill(2)}{str(year).zfill(2)}"] = -1
+            dates[f"{str(day).zfill(2)}{str(month).zfill(2)}{str(year).zfill(2)}"] = "-"
 
-non_dates_count = 0
 character_count = 5
-found_count = 0
-last_found = ""
 
 with open("digits.txt", "r") as file:
     date_str = file.read(6)
 
     while len(date_str) == 6:
-        if date_str in dates and dates[date_str] < 0:
-            dates[date_str] = character_count-6
-            found_count += 1
-            last_found = date_str
-        else:
-            non_dates_count += 1
+        if date_str in dates and dates[date_str] == "-":
+            dates[date_str] = f"PI8-{character_count-5}"
 
         date_str = date_str[1:] + file.read(1)
         character_count += 1
 
-found_dates = [key for (key, value) in dates.items() if value >= 0]
-missing_dates = [key for (key, value) in dates.items() if value < 0]
+found_dates = [key for (key, value) in dates.items() if value != "-"]
+missing_dates = [key for (key, value) in dates.items() if value == "-"]
 
 print(f"Among the first {character_count} digits of Pi:\n")
-print(f"Valid dates found: {len(found_dates)} ({100*len(found_dates)/len(dates)}%):")
-print("\n".join([" - " + date for date in found_dates[:20]]))
-print(" - ...")
-print(f"Dates missing: {len(missing_dates)} ({100*len(missing_dates)/len(dates)}%):")
-print("\n".join([" - " + date for date in missing_dates[:20]]))
-print(" - ...")
-print(f"Last date found at index {dates[last_found]}: {last_found}")
+print(f"Valid dates found: {len(found_dates)} ({100*len(found_dates)/len(dates)}%)")
+print(f"Dates missing: {len(missing_dates)} ({100*len(missing_dates)/len(dates)}%)")
+
+with open("date_conversions_PI6.txt", "w") as file:
+    for date, pi6_dates in dates.items():
+        days = date[0:2]
+        months = date[2:4]
+        years = date[4:]
+        if len(years) < 4:
+            years = "19"+years if int(years) >= 70 else "20"+years
+        file.write(days+"/"+months+"/"+years+": "+pi6_dates+"\n")
 
 
 # Digits-billion.txt
